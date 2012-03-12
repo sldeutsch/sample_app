bc877208607b6062bef45c1032b6254fc61d70b1
Sheena Deutsch <sdeutsch@drivingloyalty.com>
2012-03-12 10:02:06 -0500

    re did the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '46a8d2c9f42ce0416d57d5411ad4b4df380eca524a4d181ab1fccf70f7906baa30645d426a4b5b6b02d7d7fad615d71fd3615b25fdb2335806e0e47f3fe9b675'
