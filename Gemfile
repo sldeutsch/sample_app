c28d606ac08639a41dc336ad6dc2c38f57ebefbc
Sheena Deutsch <sdeutsch@drivingloyalty.com>
2012-04-09 10:23:13 -0500

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -3,11 +3,13 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.1'
 
 gem 'bootstrap-sass', '2.0.0'
+gem 'bcrypt-ruby', '3.0.1'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 group :development, :test do
 gem 'sqlite3', '1.3.5'
 gem 'rspec-rails', '2.8.1'
+gem 'annotate', '~> 2.4.1.beta'
 end
 
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,63 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it {should have_selector('h1',  text: heading) }
+  end
+
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    {'Sample App' }
+    let(:page_title) {''}
 
-    it { should have_selector('h1',    text: 'Sample App') }
-   
-    it { should_not have_selector 'title', text: '| Home' }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'tite', text: '| home'}
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading)    {'Sample App' }
+    let(:page_title)  {''}
 
-    it { should have_selector('h1',    text: 'Help') }
-    
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'tite', text: '| Help'}
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading)    {'Sample App' }
+    let(:page_title)  {''}
 
-    it { should have_selector('h1',    text: 'About') }
+  
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'tite', text: '| About'}
     
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading)    {'Sample App' }
+    let(:page_title)  {''}
 
-    it { should have_selector('h1',    text: 'Contact') }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'tite', text: '| Contact'}
+  end
+
+
+      it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should # fill in
+    click_link "Contact"
+    page.should # fill in
+    click_link "Home"
+    page.should # fill in
+    click_link "Sign up now!"
+    page.should # fill in
+  
    
   end
 end
